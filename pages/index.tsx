74e91956d96b28121c5c926014e6508062eb4392
mdobaid311 <mdobaid311@gmail.com>
2022-07-31 22:03:46 +0530

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
-import Head from "next/head";
-import Image from "next/image";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import Dashboard from "../components/Dashboard";
 import Loader from "../components/Loader";
 
-const Home: NextPage = () => {
+export default function Home() {
   const router = useRouter();
   const { status, data: session } = useSession({
     required: true,
@@ -14,21 +12,19 @@ const Home: NextPage = () => {
       router.push("/auth/signin");
     },
   });
-  console.log(session)
 
+  // Loading animation...
   if (status === "loading") {
     return <Loader />;
   }
 
   return (
-    <div>
+    <div className="">
       <Head>
-        <title>Spotify- Dashboard</title>
+        <title>Spotify - Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Dashboard />
     </div>
   );
-};
-
-export default Home;
+}
