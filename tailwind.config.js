f4e61be65c06aa803414057a40890ab9b8fb7fdf
mdobaid311 <mdobaid311@gmail.com>
2022-07-31 12:35:01 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,38 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": "1500px",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0.5,
+          },
+        },
+      },
+      animation: {
+        pulse: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+      sans: ["ui-sans-serif", "system-ui"],
+    },
   },
-  plugins: [],
-}
+  variants: {
+    extend: {},
+    scrollbar: ["rounded"],
+  },
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar"),
+  ],
+};
